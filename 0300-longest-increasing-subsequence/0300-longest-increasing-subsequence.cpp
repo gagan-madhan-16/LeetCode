9ec8a85a80393b3c80d6b676925b0b8920214eb6
Gagan Madhan <158490863+gagan-madhan-16@users.noreply.github.com>
2025-07-02 07:51:39 +0530

    Time: 163 ms (40.16%), Space: 296.7 MB (6.44%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -45,13 +45,53 @@ private:
 
         return curr[0];
     }
+    // {10,9,2,5,3,7,101,18}
+    int printingLis(int n , vector<int> &arr){
+        vector<int> dp(n,1),idx(n,0);
+        int maxi=1,maxId=0;
+        for(int i=0;i<n;i++)
+        idx[i]=i;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i]>arr[j] && 1+dp[j]>dp[i]){
+                    idx[i]=j;
+                    dp[i]=1+dp[j];
+                    maxi=max(maxi,dp[i]);
+                    maxId=i;
+                }
+            }
+        }
+        while(true){
+            cout<<arr[maxId]<<' ';
+            maxId=idx[maxId];
+            if(maxId==idx[maxId]){
+                cout<<arr[maxId];
+                break;
+            }
+        }
+        return maxi;
+
+    }
+    int  moreOptimization(int n , vector<int> &arr){
+        vector<int> ans(n,1);
+        int maxi=0;
+        for(int i=1;i<n;i++){
+            auto it = lower_bound(arr.begin(), arr.begin()+i, arr[i]);
+            if(it!=arr.begin())
+            ans[i] = 1+ans[it-1-arr.begin()];
+            maxi=max(maxi,ans[i]);
+        }
+        return maxi;
+    }
 public:
     int lengthOfLIS(vector<int>& arr) {
         n = arr.size();
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
         // int ans = memoisation(-1,0,dp,arr);
         // int ans = tabulation(n,arr);
-        int ans = SpaceOptimiztionTabulation(n,arr);
+        // int ans = SpaceOptimiztionTabulation(n,arr);
+        int ans = printingLis(n,arr);
+        // int ans = moreOptimization(n,arr);
 
         return ans;
        
